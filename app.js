1fad44faf1f2fc2b38b27766590682b699bb6e32
ivan5761 <164543289+ivan5761@users.noreply.github.com>
2024-03-22 11:38:09 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.Log("hola")
+//variables 
+let numeroMaximo = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximo)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabrasVeces = 'vez';
+let maximosIntentos = 6;
+while (numeroUsuario != numeroSecreto) {
+ numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximo} por favor:`));
+
+    console.log(numeroUsuario);
+    /* 
+    este codigo realiza 
+    la comparacion
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste el numero es: ${numeroUsuario}. lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else {
+    if(numeroUsuario > numeroSecreto) {
+        alert('el numero secreto es menor');
+    } else {
+        alert('el numero secreto es mayor');
+    }
+   // intentos = intentos + 1;
+   //intentos += 1;
+   intentos++;
+
+   // palabrasVeces = 'veces';
+    if (intentos > maximosIntentos) {
+        alert(`llegaste al numero maximo de ${maximosIntentos} intentos`);
+        break;
+    }
+    }
+}
